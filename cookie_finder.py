4ea9b1512b89ba248228672513a0c4ffceee2a72
Mohan Xu <mohanxu@berkeley.edu>
2026-05-04 11:17:49 -0700

    small issues fix

--- cookie_finder.py
+++ cookie_finder.py
@@ -5,12 +5,14 @@ from collections import defaultdict
 def find_most_active_cookies(date, cookie_rows):
     #key = cookie, value = count for specific date 
     counts = defaultdict(int)
+    
     for row in cookie_rows:
         cookie, timestamp = row.strip().split(",")
         row_date = timestamp.split("T")[0]
         if row_date == date:
             counts[cookie] += 1
-    
+    if not counts:
+        return []
     max_count = max(counts.values())
     return [cookie for cookie, count in counts.items() if count == max_count]
 
---
+++ test_cookie.py
@@ -0,0 +1,1 @@
+#handles unit tests and such 
